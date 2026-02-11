6fb66a5447bb68ca9479ce2ca612cb065269fa67
Berndoe <88336484+Berndoe@users.noreply.github.com>
2026-02-11 01:46:45 +0000

    fixed character movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -19,31 +19,25 @@ public class Player : MonoBehaviour
 
     private Vector3 movementDirection;
 
-    void Awake()
-    { 
-        cameraTransform = Camera.main.transform;
-        characterController = GetComponent<CharacterController>();
-        characterAnimator = GetComponent<Animator>();
-    }
     // Start is called before the first frame update
     void Start()
     {
+        cameraTransform = Camera.main.transform;
+        characterController = GetComponent<CharacterController>();
+        characterAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveCharacter();
-        animateCharacter();
-        // Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-        // characterController.Move(movement * Time.deltaTime * characterSpeed);
+        MoveCharacter();
     }
 
     /**
         * This method is responsible for moving the character based on player input.
         * It handles both horizontal movement and jumping mechanics.
     */
-    private void moveCharacter()
+    void MoveCharacter()
     {
         // Getting input from the player
         float horizontalAxis = Input.GetAxis("Horizontal");
@@ -51,8 +45,10 @@ public class Player : MonoBehaviour
 
         Vector3 normalizedMovement = new Vector3(horizontalAxis, 0, verticalAxis).normalized;
 
-        // Normal player movement
-        if (normalizedMovement.magnitude > 0.1f)
+        bool isRunning = normalizedMovement.magnitude > 0.1f;
+        characterAnimator.SetBool("IsRunning", isRunning);
+
+        if (isRunning)
         {
           float targetAngle =
            Mathf.Atan2(normalizedMovement.x, normalizedMovement.z) * Mathf.Rad2Deg
@@ -60,9 +56,14 @@ public class Player : MonoBehaviour
 
            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
            movementDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-
-           characterController.Move(movementDirection * characterSpeed * Time.deltaTime);
         }
+
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+
+        characterController.Move(movementDirection * characterSpeed * Time.deltaTime);
 
         // Apply gravity
         if (characterController.isGrounded && verticalVelocity < 0)
@@ -71,7 +72,7 @@ public class Player : MonoBehaviour
         }
       
         verticalVelocity += gravity * Time.deltaTime;
-        characterController.Move(Vector3.up * verticalVelocity * Time.deltaTime);
+        characterController.Move(Time.deltaTime * verticalVelocity * Vector3.up);
       
         // Jump action
         if (Input.GetButtonDown("Jump") && characterController.isGrounded)
@@ -82,10 +83,4 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void animateCharacter()
-    {
-        float currentCharacterSpeed = new Vector3(characterController.velocity.x, 0, characterController.velocity.z).magnitude;
-        characterAnimator.SetFloat("Speed", 1f * currentCharacterSpeed);
-        characterAnimator.SetBool("IsGrounded", characterController.isGrounded);
-    }
 }
